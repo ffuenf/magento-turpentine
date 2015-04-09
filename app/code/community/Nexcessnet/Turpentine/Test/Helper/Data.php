94a85766c17f446db593687ef4babdd58429715c
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2015-04-09 17:20:03 +0200

    add testing

---
+++ app/code/community/Nexcessnet/Turpentine/Test/Helper/Data.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+* Test for class Nexcessnet_Turpentine_Helper_Data
+*
+* @category    Nexcessnet
+* @package     Nexcessnet_Turpentine
+*/
+class Nexcessnet_Turpentine_Test_Helper_Data extends EcomDev_PHPUnit_Test_Case
+{
+  /**
+  * Tests extension version
+  *
+  * @test
+  * @loadFixture
+  */
+  public function testExtensionVersion()
+  {
+    $this->assertEquals(
+      Mage::helper('turpentine')->getVersion(),
+      '0.6.1'
+    );
+  }
+}
