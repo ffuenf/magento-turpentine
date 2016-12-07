d929b851348377dcb8995dfcfb6d7189df47c95e
Lärsle <Lars@Malach.de>
2016-12-07 04:33:17 +0100

    [KOEMPF] Correct path to abstract.php
    abstract.php can not be loaded, because __DIR__ points to the composer path vendor/flagbit/magento-turpentine/shell/cron/

--- shell/cron/turpentineUrlCacheStatus.php
+++ shell/cron/turpentineUrlCacheStatus.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'abstract.php';
+require_once '..' . DIRECTORY_SEPARATOR . 'abstract.php';
 
 class TurpentineUrlCacheStatus extends Mage_Shell_Abstract
 {
